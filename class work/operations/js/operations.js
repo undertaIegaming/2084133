622330a71c065fb83fedf6974bcd9501855d3cf6
doseone_ <149911589+DoseTwo@users.noreply.github.com>
2024-05-02 10:53:23 -0300

    sigh

---
+++ class work/operations/js/operations.js
@@ -0,0 +1,14 @@
+let x = 2;
+let y = 3;
+//never used lol
+let z = 4;
+
+let a = x + 1;
+let b = x + y;
+let c = x - y;
+let d = x * y;
+let e = x / y;
+let f = 5 % 3;
+let g = x ** 3;
+
+alert(" a = " + a + "\n b = " + b + "\n c = " + c + "\n d = " + d + "\n e = " + e + "\n f = " + f + "\n g = " + g + "\n a==b? = " + (a==b));
