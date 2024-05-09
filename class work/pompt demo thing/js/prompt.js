544925c0a9a3103564f5708e03e04fbb10fd2281
doseone_ <149911589+DoseTwo@users.noreply.github.com>
2024-05-09 18:50:57 -0300

    somehting

--- class work/pompt demo thing/js/prompt.js
+++ class work/pompt demo thing/js/prompt.js
@@ -1,13 +1,24 @@
 let a, b, c, delta, x1, x2;
 
-a = prompt("A = ");
-b = prompt("B = ");
-c = prompt("C = ");
+function sanitise(x)
+{
+	do 
+	{
+		x = prompt("Donner une valeur);
+		if (isNaN(x))
+			alert("Invalid Input");
+	} while(isNaN)
+	return x
+}
+
+a = sanitise("A = ");
+b = sanitise("B = ");
+c = sanitise("C = ");
 console.log(a, b, c);
 delta = (b ** 2) - (4 * a * c);
 
 if (delta < 0)
-	console.log("fart");
+	console.log("Aucune Solution Entiere");
 else
 {
 	if(delta == 0)
